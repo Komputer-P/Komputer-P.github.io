51d952f610e3decaf6e68623f0ba1124b1837a43
Komputer-P <wlghks0314@gmail.com>
2021-01-13 15:31:29 +0900

    JQuery Implemented

---
+++ colors.js
@@ -0,0 +1,38 @@
+var Link = {
+  setColor:function (color) {
+    // var alist = document.querySelectorAll('a');
+    // for(var i=0; i<alist.length; i++) {
+    //   console.log(alist[i]);
+    //   alist[i].style.color = color;
+    // }
+    $('a').css('color', color);
+  }
+}
+
+var Body = {
+    setColor:function (color) {
+      //document.querySelector('body').style.color = color;
+      $('body').css('color', color);
+    },
+    setBackgroundColor:function(color) {
+      //document.querySelector('body').style.backgroundColor = color;
+      $('body').css('backgroundColor', color);
+    }
+}
+
+function nightdayHandler(self) {
+    var target = document.querySelector('body');
+    if(self.value === 'night') {
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value = 'day';
+
+      Link.setColor('powderblue');
+    } else {
+      Body.setBackgroundColor('white');
+      Body.setColor("black");
+      self.value = 'night';
+
+      Link.setColor('blue');
+    }
+}
