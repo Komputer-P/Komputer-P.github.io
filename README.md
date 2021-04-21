8c44241110d593cb48491d1e8505a2de4437121f
Komputer-P <wlghks0314@gmail.com>
2021-04-22 01:48:40 +0900

    React Study Completed

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 For Web Programming Study
 
 # Studied
-   + Bascis
-       + HTML5
-       + CSS3
-       + Vanilla JavaScript (ES6)
-   + Back-end Frameworks
-       + Node.js Web Server
++ Bascis
+    + HTML5
+    + CSS3
+    + Vanilla JavaScript (ES6)
+
++ Front-end Frameworks
+    + React.js
+    
++ Back-end Frameworks
+    + Node.js Web Server
--- React_js/src/components/TOC.js
+++ React_js/src/components/TOC.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    render() {
-      var lists = [];
-      var data = this.props.data;
-      var i = 0;
-
-      while(i < data.length) {
-        lists.push(
-          <li key={data[i].id}>
-            <a
-              href={"/content/"+data[i].id}
-              data-id={data[i].id}
-              onClick={function(e) {
-                e.preventDefault();
-                this.props.onChangePage(e.target.dataset.id);
-              }.bind(this)}>
-                {data[i].title}</a>
-          </li>)
-
-        i = i+1;
-      }
-      return (
-        <nav>
-        <ul>
-          {lists}
-        </ul>
-        </nav>   
-      );
+  shouldComponentUpdate(updatedProps, updatedState) {
+    if(updatedProps.data === this.props.data) {
+      console.log(" no render!");
+      return false;
+    }
+    else {
+      console.log("render!");
+      return true;
     }
   }
 
-  export default TOC;
+  render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}>
+            {data[i].title}</a>
+        </li>)
+
+      i = i + 1;
+    }
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC;
